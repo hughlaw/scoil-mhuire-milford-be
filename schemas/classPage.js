ded9759d6329f0eef60cdf1e916f97fceac11695
Hugh Law <hugh.law@terminalfour.com>
2020-10-10 19:33:18 +0100

    add sorting on class pages

--- schemas/classPage.js
+++ schemas/classPage.js
@@ -5,12 +5,19 @@ export default {
   title: 'Class page',
   type: 'document',
   icon: FiClipboard,
+  orderings: [
+    {
+      title: 'Sort ordering',
+      name: 'sortOrderAsc',
+      by: [{ field: 'sortOrder', direction: 'asc' }],
+    },
+  ],
   fields: [
     {
       name: 'title',
       title: 'Page title',
       type: 'string',
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'slug',
@@ -18,7 +25,7 @@ export default {
       title: 'Page URL',
       description:
         'A slug is a generated string of text used to create unique URL for this page',
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
       options: {
         source: 'title',
       },
@@ -33,7 +40,58 @@ export default {
       name: 'pageContent',
       title: 'Page content',
       type: 'sm-block',
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'sortOrder',
+      title: 'Sort ordering',
+      description:
+        'Use this to sort the order of these pages when on the classes page',
+      type: 'number',
+      options: {
+        list: [
+          {
+            value: 1,
+            title: '1 - Top',
+          },
+          {
+            value: 2,
+            title: '2',
+          },
+          {
+            value: 3,
+            title: '3',
+          },
+          {
+            value: 4,
+            title: '4',
+          },
+          {
+            value: 5,
+            title: '5',
+          },
+          {
+            value: 6,
+            title: '6',
+          },
+          {
+            value: 7,
+            title: '7',
+          },
+          {
+            value: 8,
+            title: '8',
+          },
+          {
+            value: 9,
+            title: '9',
+          },
+          {
+            value: 10,
+            title: '10 - Bottom',
+          },
+        ],
+      },
     },
   ],
 };
