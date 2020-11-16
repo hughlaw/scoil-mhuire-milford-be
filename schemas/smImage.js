53d8515550a21028af3a77a96fe18bbd30228c94
Hugh Law <hugh.law@terminalfour.com>
2020-11-16 21:23:34 +0000

    update images in slideshows and image content to allow hotspot and crop functionality

--- schemas/smImage.js
+++ schemas/smImage.js
@@ -6,6 +6,9 @@ export default {
     {
       name: 'image',
       type: 'image',
+      options: {
+        hotspot: true,
+      },
     },
     {
       name: 'alt',
@@ -13,7 +16,7 @@ export default {
       description:
         'Provide some descriptive text for this image for sight impaired users. e.g. "A stack of books placed on a desk" - Don\'t include phrases like "Picture of a" as these are not required.',
       type: 'string',
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
     },
   ],
 };
